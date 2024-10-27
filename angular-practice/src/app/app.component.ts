2c3d8c31646d36d8204593deb653dcdc63d3a266
Cristian-Veliz <cristianveliz6@gmail.com>
2024-10-26 23:04:51 -0300

    update1

--- angular-practice/src/app/app.component.ts
+++ angular-practice/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
+@Component({ 
   selector: 'app-root',
-  standalone: true,
+  standalone: true, 
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -11,3 +11,6 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular-practice';
 }
+
+// @Component es un decorador
+// standalone: true indica que no va a tener modulo , de la version 17 en adelante se crea asi, en versiones anteriores se crean con modulo. 
